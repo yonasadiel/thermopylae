c4eeeb90d4a7fe4dbbe194321835c02a549b5276
Wen Junhua <wenjh1998@gmail.com>
2024-06-17 15:42:48 +0800

    feat: add settings tab for particle settings

--- src/components/settings/Background.tsx
+++ src/components/settings/Background.tsx
@@ -1,5 +1,5 @@
-import { useSettings } from '../../hooks';
-import './Background.css';
+import { useSettings } from "../../hooks";
+import "./Background.css";
 
 const BackgroundSettings = () => {
     const { settings, setSettingValue } = useSettings();
@@ -10,7 +10,11 @@ const BackgroundSettings = () => {
                 <label>Background Image</label>
                 <input
                     type="file"
-                    onChange={(e) => setSettingValue('backgroundImagePath', e.target.value)} value={settings.backgroundImagePath} />
+                    onChange={(e) =>
+                        setSettingValue("backgroundImagePath", e.target.value)
+                    }
+                    value={settings.backgroundImagePath}
+                />
             </div>
             <div className="input-group">
                 <label>Particles</label>
@@ -18,12 +22,37 @@ const BackgroundSettings = () => {
                     <input
                         type="checkbox"
                         checked={settings.backgroundParticlesEnabled}
-                        onChange={(e) => setSettingValue('backgroundParticlesEnabled', !!e.currentTarget.checked)} />
+                        onChange={(e) =>
+                            setSettingValue(
+                                "backgroundParticlesEnabled",
+                                !!e.currentTarget.checked,
+                            )
+                        }
+                    />
                     <span>Enabled</span>
                 </p>
             </div>
+            <div className="input-group">
+                <label>Particles Settings</label>
+                <p>
+                    You can import your own settings from{" "}
+                    <a
+                        href="https://vincentgarreau.com/particles.js/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >here</a>
+                </p>
+                <textarea
+                    onChange={(e) =>
+                        setSettingValue(
+                            "backgroundParticlesConfig",
+                            JSON.parse(e.currentTarget.value),
+                        )}
+                    value={JSON.stringify(settings.backgroundParticlesConfig)}
+                />
+            </div>
         </div>
     );
-}
+};
 
 export default BackgroundSettings;
