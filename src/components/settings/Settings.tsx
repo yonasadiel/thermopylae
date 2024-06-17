403369aae79788e747adf5e92a6ab311d75b5328
Yonas Adiel Wiguna <yonazadielwiguna@gmail.com>
2024-06-17 21:27:38 +0800

    Styling for colors

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import BackgroundSettings from './Background';
+import ThemeSettings from './Theme';
 import QuoteSettings from './Quote';
 import './Settings.css';
 
@@ -7,26 +7,27 @@ export interface SettingsProps {
     onClose: () => void
 }
 
-const MENU_BACKGROUND = 'background';
+const MENU_THEME = 'theme';
 const MENU_QUOTE = 'quote';
 const MENU_TERMINAL = 'terminal';
 
 export default function Settings(props: SettingsProps) {
     const { onClose } = props;
-    const [menu, setMenu] = useState(MENU_BACKGROUND);
+    const [menu, setMenu] = useState(MENU_THEME);
     return (
         <div className="settings">
             <div className="close-button" onClick={() => onClose()}>
+                {/* TODO: make this custom color */}
                 <img alt="cross" src="/assets/cross.svg" width="20px" height="20px" />
             </div>
             <div className="navbar">
-                <div className={`menu ${menu === MENU_BACKGROUND ? 'active' : ''}`} onClick={() => setMenu(MENU_BACKGROUND)}>Background</div>
+                <div className={`menu ${menu === MENU_THEME ? 'active' : ''}`} onClick={() => setMenu(MENU_THEME)}>Theme</div>
                 <div className={`menu ${menu === MENU_QUOTE ? 'active' : ''}`} onClick={() => setMenu(MENU_QUOTE)}>Quote</div>
                 <div className={`menu ${menu === MENU_TERMINAL ? 'active' : ''}`} onClick={() => setMenu(MENU_TERMINAL)}>Terminal</div>
             </div>
             <div className="divider" />
             <div className="settings-content">
-                {menu === MENU_BACKGROUND && <BackgroundSettings />}
+                {menu === MENU_THEME && <ThemeSettings />}
                 {menu === MENU_QUOTE && <QuoteSettings />}
                 {menu === MENU_TERMINAL && <TerminalSettings />}
             </div>
